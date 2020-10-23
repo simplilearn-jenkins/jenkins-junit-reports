eb157c25b0b139a9314132f14db49d36458d3941
student <vinodh.mahendra@gmail.com>
2020-10-23 10:20:10 +0000

    JUnit Test

---
+++ src/test/java/com/simplilearn/workshop/JenkinsJunitDemo.java
@@ -0,0 +1,59 @@
+package com.simplilearn.workshop;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+
+public class JenkinsJunitDemo
+{
+    private static String Base_Url = "https://www.facebook.com";
+    private WebDriver driver;
+
+    @Before
+    public void setUp() 
+    {
+    	System.setProperty("webdriver.gecko.driver","/home/labsuser/geckodriver");
+        driver = new FirefoxDriver();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--headless");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);				
+	
+        
+        driver.get(Base_Url);
+    }
+
+    @After
+    public void after()
+    {
+        driver.quit();
+    }
+
+    @Test
+    public void testCasePassed()
+    {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='u_0_a']")).isDisplayed());
+    }
+
+    @Test
+    public void testCaseFailed()
+    {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='failed case']")).isDisplayed());
+    }
+
+    @Ignore
+    @Test
+    public void testCaseIgnored()
+    {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='ignored case']")).isDisplayed());
+    }
+}
